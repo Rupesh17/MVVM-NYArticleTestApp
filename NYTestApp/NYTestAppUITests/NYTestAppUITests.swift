08e01faf05269d7f058b34613837f9b909538254
rupesh17 <soft.rup17@gmail.com>
2018-07-09 10:50:26 +0530

    Xcode server + fastlane
    Xcode server + fastlane

--- NYTestApp/NYTestAppUITests/NYTestAppUITests.swift
+++ NYTestApp/NYTestAppUITests/NYTestAppUITests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class NYTestAppUITests: XCTestCase {
         
+    var app = XCUIApplication()
+    
     override func setUp() {
         super.setUp()
         
@@ -18,8 +20,9 @@ class NYTestAppUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -28,9 +31,15 @@ class NYTestAppUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAppUI() {
+        snapshot("App Screenshot Launch with indicator")
+        snapshot("before load", waitForLoadingIndicator: false)
+        snapshot("after load", waitForLoadingIndicator: true)
+        let cells = app.tables.cells
+        
+        let firstCell = cells.element(boundBy: 0)
+        firstCell.tap()
+        snapshot("detail view")
     }
     
 }
